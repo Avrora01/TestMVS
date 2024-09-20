17060beaebd0511ca45ce8e6cf46f529ebfc96ad
Sabrina <suhrobzodasabrina01@gmail.com>
2024-09-20 10:44:07 +0500

    first commit

---
+++ Test/Pages/Terms.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test.Pages
+{
+    public class TermsModel : PageModel
+    {
+        private readonly ILogger<TermsModel> _logger;
+  
+        public TermsModel(ILogger<TermsModel> logger)
+        {
+           _logger = logger;
+        }
+   
+         public void OnGet()
+        {
+
+        }
+    }   
+}
+
